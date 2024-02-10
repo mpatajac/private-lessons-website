5878c70a53f3f3eb480ae7350d352e1416af92eb
mpatajac <patajacmatija3@gmail.com>
2024-02-10 19:56:55 +0100

    add logic to convert markdown to html

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,3 +1,25 @@
-pub fn test_func(a: i32, b: i32) -> i32 {
-    a + b
+mod markdown {
+    pub fn to_html(content: &str) -> Result<String, String> {
+        markdown::to_html_with_options(content, &markdown::Options::gfm())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    // tests already panic on `assert`s, so we might as well use unwrap.
+    #![allow(clippy::unwrap_used)]
+
+    use super::*;
+
+    #[test]
+    fn test_md_to_html() {
+        let md = "# Test post\n\nThis is a **test post** in which I test the capabilities of the [markdown conversion lib](https://crates.io/crates/markdown/1.0.0-alpha.16).";
+
+        let expected_html = "<h1>Test post</h1>\n<p>This is a <strong>test post</strong> in which I test the capabilities of the <a href=\"https://crates.io/crates/markdown/1.0.0-alpha.16\">markdown conversion lib</a>.</p>";
+
+        let received_html = markdown::to_html(md);
+
+        assert!(received_html.is_ok());
+        assert_eq!(received_html.unwrap(), expected_html);
+    }
 }
--- builder/src/main.rs
+++ builder/src/main.rs
@@ -1,6 +1,3 @@
-use builder::test_func;
-
 fn main() {
-    let test_result = test_func(2, 3);
-    println!("{}", test_result);
+    println!("Hello, world!");
 }
