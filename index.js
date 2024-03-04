2d2a33ef0391a7b3e39df026a1b8a5a35955c0e4
mpatajac <patajacmatija3@gmail.com>
2024-03-04 21:58:16 +0100

    remove navbar underline before scrolling (to "connect" with hero)

---
+++ index.js
@@ -0,0 +1,19 @@
+window.onscroll = () => {
+	handleNavbarUnderline();
+}
+
+
+
+// Add or remove navbar shadow depending on whether page is scrolled or not
+function handleNavbarUnderline() {
+	// NOTE: one (and only one) navbar should exist, so indexing should be fine
+	const navbar = document.getElementsByClassName("navbar")[0];
+
+	// window.scrollY > 0 => page was scrolled from initial position
+	// shadow should be present when page is scrolled
+	if (window.scrollY > 0) {
+		navbar.classList.add("has-shadow");
+	} else {
+		navbar.classList.remove("has-shadow");
+	}
+}
