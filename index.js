700c2c86aec88b56edf844905ef50dd092d9fb9d
mpatajac <patajacmatija3@gmail.com>
2024-03-05 17:02:53 +0100

    implement animation-on-scroll

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-window.onscroll = () => {
-	handleNavbarUnderline();
+function setup() {
+	// TODO?: collect all `aos` elements here, pass them to `applyAOS` to prevent re-fetching on every scroll?
+	window.onscroll = () => {
+		handleNavbarUnderline();
+		applyAOS();
+	}
+
+	// check on page load if any elements need to be shown
+	applyAOS();
 }
 
 
@@ -22,4 +29,43 @@ function handleNavbarUnderline() {
 // Apply (smooth) scrolling back to the top of the page
 function scrollToTop() {
 	document.documentElement.scrollTop = 0;
+}
+
+
+// Apply logic so specified elements can perform animation when they are scrolled to
+function applyAOS() {
+	// used to delay element display (wait until it has scrolled enough)
+	// const checkDelta = window.screen.availHeight / 4;
+	const checkDelta = 200;
+	const isElemVisible = (elem) => {
+		// `elem.getBoundingClientRect().top` gets elements distance from the top of the viewport
+		// so we need to check if the top has reached viewport bottom (- delta)
+		const viewportBottom = window.screen.availHeight - checkDelta;
+		const elementTop = elem.getBoundingClientRect().top;
+
+		return viewportBottom > elementTop;
+	};
+
+	// singular elements
+	const aosElems = document.getElementsByClassName("aos");
+	for (const elem of aosElems) {
+		if (isElemVisible(elem)) {
+			elem.classList.add("animated");
+		}
+	}
+
+	// element collections
+	const aosCollectionElems = document.getElementsByClassName("aos-collection");
+	for (const parentElem of aosCollectionElems) {
+		let childElems = parentElem.children;
+
+		for (let i = 0; i < childElems.length; ++i) {
+			const elem = childElems[i];
+
+			if (isElemVisible(elem)) {
+				// add index alongside class to delay animation
+				elem.classList.add("animated", `animated-stagger-${i}`);
+			}
+		}
+	}
 }
